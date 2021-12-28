2c14e4e5e03b4549eff19815553b9584ce3fd2f7
JtkPL <noemail@gmail.com>
2021-12-27 20:41:23 -0500

    Implementação inicial  Script brute-force de subdomínios.

---
+++ sub_domain.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo ""
+echo "[ ** BRUTE FORCE SUB DOMAIN ** ]"
+echo ""
+
+if [ "$1" != "" ]
+then
+    verify_domain=$(host $1 | grep "NXDOMAIN" | cut -d ")" -f1 | cut -d "(" -f2)
+fi
+
+if [ "$1" = "" -o "$2" = "" -o "$verify_domain" = "NXDOMAIN" ]
+then
+    echo "usage: ./sub_domain <domain> <wordlist>"
+    echo ""
+else
+    echo "" > file.txt
+
+    for prefix_sub in $(cat $2)
+    do
+        if [ "$prefix_sub" != "" ]
+        then 
+            sub_domain=$(host -t A $prefix_sub.$1 | grep -v "NXDOMAIN" | cut -d " " -f1) 
+            
+            if [ "$sub_domain" != "" ]
+            then 
+                ip_sub_domain=$(host -t A $sub_domain | cut -d " " -f4)
+                echo "$sub_domain : $ip_sub_domain" >> file.txt
+            fi
+            
+        fi
+    done 
+fi
